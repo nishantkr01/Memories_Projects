f564ba5f0f9cdff7db4c008cc96bc60a528d7d48
Amit <nishantkr12@outlook.com>
2021-09-17 16:12:41 +0530

    first commit

---
+++ client/src/reducers/posts.js
@@ -0,0 +1,9 @@
+ const reducer = (posts=[], action) => {
+    switch (action.type) {
+        case 'FETCH_ALL':
+            return posts;
+        case 'CREATE':
+            return posts;
+        default:
+            return posts;    }
+} 
---
+++ server/index.js
@@ -0,0 +1,27 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+
+import postRoutes from './routes/posts.js';
+
+const app = express();
+
+app.use('/posts', postRoutes);
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+const CONNECTION_URL = 'mongodb+srv://reactmastery:reactmastery123@cluster0.rvix6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, {useNewUrlParser:true, useUnifiedTopology: true})
+.then(()=> app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+.catch((error)=> console.log(error. message));
+
+
+
+// mongoose.set('useFindAndModify', false);
+
+// https://www.mongodb.com/cloud/atlas
